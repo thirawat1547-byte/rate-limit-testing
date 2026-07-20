9c1c9e00a9b4b96b38d8601cd0822c4c0741d4e3
thirawat1547-byte <thirawat1547@gmail.com>
2026-07-20 16:12:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import hashlib
 import hmac
 import secrets
@@ -9,7 +11,15 @@ app = Flask(__name__)
 # แสดง JSON ตามลำดับที่เขียนไว้ใน Dictionary
 app.json.sort_keys = False
 
-WORK_FACTOR = 2_000_000 ##---------##
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+    default_limits=[],
+    storage_uri="memory://",
+    headers_enabled=True
+)
+
+WORK_FACTOR = 2000 ##---------##
 PASSWORD_LENGTH = 10
 SALT_SIZE_BYTES = 16
 
@@ -41,6 +51,7 @@ def home():
 
 
 @app.route("/login-check")
+@limiter.limit("5 per second")
 def login_check():
     start_time = time.perf_counter()
 
